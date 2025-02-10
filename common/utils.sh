1c4243a989b2c68da7627bb6795de8748d43e69d
Dakkshesh <dakkshesh5@gmail.com>
2025-02-10 18:12:16 +0530

    common: utils: Bump binutils to 2.44
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- common/utils.sh
+++ common/utils.sh
@@ -20,7 +20,7 @@ CURR_DIR=$(pwd)
 
 # Set versions
 export AVR_LIBC_VER="2.2.1-clang"
-export BINUTILS_VERSION="2.43.1"
+export BINUTILS_VERSION="2.44"
 export GCC_VER="14.2.0"
 export NEWLIB_VER="4.5.0.20241231"
 export LLVM_VERSION="20.1.0-rc1"
